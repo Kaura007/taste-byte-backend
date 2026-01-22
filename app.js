a054ecb7800befd1cca7d72c8a927be15f2de011
Mohammed Abdulrahman kaura <81968559+Kaura007@users.noreply.github.com>
2026-01-22 12:55:33 +0100

    Add API endpoint to check server status

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
 app.use("/api", rootRouter);
 app.use("/api/data", seedMenu);
 app.use("/api/food", foodRoutes);
